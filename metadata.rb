72405171caabbd2acdf144f69349ec853469bbfd
VBARobotics <41477383+TXCANDude@users.noreply.github.com>
2019-06-14 18:58:37 -0500

    Added default.rb and renamed arm recipe

--- metadata.rb
+++ metadata.rb
@@ -4,17 +4,17 @@ maintainer_email 'lsquire@gmail.com'
 license 'All Rights Reserved'
 description 'This is a series of recipes that will configure a Raspbery Pi for various Robotics Projects'
 long_description 'Installs/Configures vba_robotics'
-version '0.3.3'
+version '0.3.4'
 chef_version '>= 14.0'
 
 # The `issues_url` points to the location where issues for this cookbook are
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
 # uploaded to a Supermarket.
 #
-# issues_url 'https://github.com/<insert_org_here>/vba_robotics/issues'
+# issues_url 'https://github.com/VBARobtics/vba_robotics/issues'
 
 # The `source_url` points to the development repository for this cookbook.  A
 # `View Source` link will be displayed on this cookbook's page when uploaded to
 # a Supermarket.
 #
-# source_url 'https://github.com/<insert_org_here>/vba_robotics'
+# source_url 'https://github.com/VBARobotics/vba_robotics'
