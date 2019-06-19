8a5ed918772a07832317b38866a75c417cd32369
VBARobotics <41477383+TXCANDude@users.noreply.github.com>
2019-06-18 20:28:17 -0500

    Fixed commad error

--- recipes/display.rb
+++ recipes/display.rb
@@ -1,5 +1,5 @@
 #Raspberry Pi 3.5" touchscreen install
-xecute 'Clone Driver Repository' do
+execute 'Clone Driver Repository' do
   command 'https://github.com/waveshare/LCD-show.git&&cd LCD-show/&&./LCD35B-show-V2'
   action:run
 end
