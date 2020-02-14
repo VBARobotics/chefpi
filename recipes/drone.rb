65b65a32101239807355a0eb4ff86b09d5b9e3c9
VBARobotics <45722764+VBARobotics@users.noreply.github.com>
2020-02-14 02:10:08 +0000

    Update drone.rb

--- recipes/drone.rb
+++ recipes/drone.rb
@@ -1,7 +1,7 @@
 #Install Pyparrot
 
 execute 'Clone PyParrot Repository' do
-  command 'cd /home/pi&&git clone https://github.com/amymcgovern/pyparrot'
+  command 'cd /home/pi&&git clone https://github.com/amymcgovern/pyparrot&&chown 777 ~/pyparrot'
   action:run
 end
 execute 'Install Pyparrot' do
