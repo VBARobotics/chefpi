155f8ffd3b10900fd1e072d2df65a09adf3110ea
David Squire <dsquire@vbainfo.com>
2020-02-24 21:22:05 -0600

    Added drone pre-reqs

--- recipes/drone.rb
+++ recipes/drone.rb
@@ -4,7 +4,14 @@ execute 'Clone PyParrot Repository' do
   command 'cd /home/pi&&git clone https://github.com/amymcgovern/pyparrot&&chown 777 /home/pi/pyparrot'
   action:run
 end
-execute 'Install Pyparrot' do
-  command 'pip3 install pyparrot'
+
+execute 'Install Pyparrot Pre-Reuqisites' do
+  command 'apt-get install bluetooth bluez python-bluez libglib2.0-dev ffmpeg vlc'
   action:run
 end
+
+execute 'Install Pyparrot' do
+  command 'pip3 install untangle zeroconf pyparrot bluepy'
+  action:run
+end
+
