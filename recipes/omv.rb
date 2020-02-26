88ccc032ae008a6c13c7b3150391b9aad679439a
VBARobotics <45722764+VBARobotics@users.noreply.github.com>
2020-02-25 20:16:46 -0600

    Update omv.rb

--- recipes/omv.rb
+++ recipes/omv.rb
@@ -1,1 +1,4 @@
-wget -O - https://github.com/OpenMediaVault-Plugin-Developers/installScript/raw/master/install | sudo bash
+execute 'Install Open Media Vault' do
+  command 'wget -O - https://github.com/OpenMediaVault-Plugin-Developers/installScript/raw/master/install | sudo bash'
+  action:run
+end
