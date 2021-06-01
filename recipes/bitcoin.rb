38e739b1241c0e5f2b8b360ef1b92b2a771f15a8
David Squire <dr.squire@protonmail.com>
2021-05-31 20:50:36 -0500

    Added fstab command

--- recipes/bitcoin.rb
+++ recipes/bitcoin.rb
@@ -11,6 +11,11 @@ directory '/mnt/bitcore' do
     mode '0777'
     action :create
 end
+execute 'Edit Fstab' do
+    command 'echo \"UUID=f3aeb9a9-d201-4ca9-9806-47150dca98e0 /mnt/bitcore ext4 defaults,nofail,x-systemd.device-timeout=2 1 1\" >> /etc/fstab'
+    action:run
+end
+/etc/fstab UUID=f3aeb9a9-d201-4ca9-9806-47150dca98e0 /mnt/bitcore ext4 defaults 1 1
 
 remote_file '/home/pi/bitcoin-0.20.1-arm-linux-gnueabihf.tar.gz' do
     source 'https://bitcoin.org/bin/bitcoin-core-0.20.1/bitcoin-0.20.1-arm-linux-gnueabihf.tar.gz'
@@ -38,4 +43,5 @@ end
 execute 'Install Missing Packages' do
     command 'apt-get install qt4-dev-tools libprotobuf-dev protobuf-compiler libqrencode-dev -y --fix-missing'
     action:run
+    ignore_failure true
 end
